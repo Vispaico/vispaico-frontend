e12a1a5cadd1f85940db2ae3bfed0f678e128682
KinhCode <61858392+KinhCode@users.noreply.github.com>
2025-08-15 17:20:37 +0700

    Deleted files related to the Niels admin panel and stories functionality.

--- src/app/admin_niels/new/page.tsx
+++
@@ -1,6 +0,0 @@
-import ArticleForm from '@/components/ArticleForm';
-import { createArticle } from '../actions';
-
-export default function NewArticlePage() {
-  return <ArticleForm action={createArticle} />;
-}
