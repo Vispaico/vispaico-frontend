7c0fe1b5078e4acaefad1abf3fab9d5e67666611
KinhCode <61858392+KinhCode@users.noreply.github.com>
2025-08-27 20:05:41 +0700

    fix: Refine laser divider spacing and size

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -5,7 +5,8 @@ import React from 'react';
 
 const SectionDivider: React.FC = () => {
   return (
-    <div className="relative w-full max-w-md mx-auto h-px bg-white/10 my-4">
+    <div className="relative w-full max-w-md mx-auto h-10 my-4">
+      <div className="absolute top-1/2 left-0 w-full h-px bg-white/10"></div>
       <div className="laser-flash"></div>
     </div>
   );
